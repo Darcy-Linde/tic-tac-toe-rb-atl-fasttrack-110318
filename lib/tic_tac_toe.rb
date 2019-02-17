5991bcfe263bd88dd2f6bb1cee9fb5235f6c6a4d
Darcy Linde <lindedarcy@gmail.com>
2019-02-17 18:22:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,8 +10,14 @@ WIN_COMBINATIONS = [
   [6,4,2]  # Diagonal bottom-top
   ]
   
-#display_board
-  
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
 #input_to_index
 def input_to_index(input)
   return input.to_i - 1
