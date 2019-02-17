8b43970d3df72248c6b7d3ed28a33d1c13ed53ae
Darcy Linde <lindedarcy@gmail.com>
2019-02-17 18:15:48 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,11 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Diagonal top-bottom
+  [6,4,2]  # Diagonal bottom-top
+  ]
